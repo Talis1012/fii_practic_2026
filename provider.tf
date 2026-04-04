bac75e7ff53f5880ec88afa54f57463e80619b74
Ionuț Feraru <44212913+ionutf01@users.noreply.github.com>
2026-04-04 17:45:25 +0000

    1

--- provider.tf
+++ provider.tf
@@ -17,7 +17,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket  = "kdsjhfhdsohgiodshg6548"
+    bucket  = "ionut-bucket-cloud"
     key     = "terraform.tfstate"
     region  = "eu-north-1"
     encrypt = true
